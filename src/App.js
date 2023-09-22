ebd341393b49611cd5ca7a47104b0e3f1b20cfe2
vwakesahu <89719144+vwakesahu@users.noreply.github.com>
2023-09-22 12:24:03 +0530

    Trusted Partners

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import HeroComponent from "./components/HeroComponent";
 import Navbar from "./components/Navbar";
+import TrustedPartners from "./components/TrustedPartners";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <Navbar />
       <div className="md:p-8 md:px-48 p-7">
         <HeroComponent />
+        <TrustedPartners />
       </div>
     </div>
   );
--- src/components/TrustedPartners.jsx
+++ src/components/TrustedPartners.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import Marquee from "react-fast-marquee";
+import img1 from "../img/trusted/1.png";
+import img2 from "../img/trusted/2.png";
+import img3 from "../img/trusted/3.png";
+import img4 from "../img/trusted/4.png";
+import img5 from "../img/trusted/5.png";
+import img6 from "../img/trusted/6.png";
+import img7 from "../img/trusted/7.png";
+import img8 from "../img/trusted/8.png";
+import img9 from "../img/trusted/9.png";
+import img10 from "../img/trusted/10.png";
+import img11 from "../img/trusted/11.png";
+import img12 from "../img/trusted/12.png";
+import img13 from "../img/trusted/13.png";
+
 
 const TrustedPartners = () => {
   return (
-    <div>TrustedPartners</div>
-  )
-}
+    <div>
+      <p className=" text-2xl font-semibold">Trusted Partners</p>
+      <div className=" mt-12">
+        <Marquee>
+            <img src={img1} alt="img1" className=" h-24 ml-6"/>
+            <img src={img2} alt="img1" className=" h-24 ml-6"/>
+            <img src={img3} alt="img1" className=" h-24 ml-6"/>
+            <img src={img4} alt="img1" className=" h-24 ml-6"/>
+            <img src={img5} alt="img1" className=" h-24 ml-6"/>
+            <img src={img6} alt="img1" className=" h-24 ml-6"/>
+            <img src={img7} alt="img1" className=" h-24 ml-6"/>
+            <img src={img8} alt="img1" className=" h-24 ml-6"/>
+            <img src={img9} alt="img1" className=" h-24 ml-6"/>
+            <img src={img10} alt="img1" className=" h-24 ml-6"/>
+            <img src={img11} alt="img1" className=" h-24 ml-6"/>
+            <img src={img12} alt="img1" className=" h-24 ml-6"/>
+            <img src={img13} alt="img1" className=" h-24 ml-6"/>
+        </Marquee>
+      </div>
+    </div>
+  );
+};
 
-export default TrustedPartners
+export default TrustedPartners;
