ea5c849a2c69fe2828e6b4a3318de8295c02547c
vwakesahu <89719144+vwakesahu@users.noreply.github.com>
2023-10-01 23:58:09 +0530

    Working Social Icons

--- src/components/SocialIcons.jsx
+++ src/components/SocialIcons.jsx
@@ -1,25 +1,21 @@
 import React from "react";
-import {
-  BiLogoMedium,
-  BiLogoLinkedin,
-  BiLogoTelegram,
-} from "react-icons/bi";
+import { BiLogoMedium, BiLogoLinkedin, BiLogoTelegram } from "react-icons/bi";
 import { FaXTwitter } from "react-icons/fa6";
 const SocialIcons = () => {
   return (
     <div className="flex gap-3">
       <a href="https://twitter.com/web3jobshunt">
-        <div className=" rounded-full border p-2">
-          <FaXTwitter alt="twitter" className=" cursor-pointer text-sm" />
+        <div className=" rounded-full border p-2 hover:bg-white hover:text-lightBlue">
+          <FaXTwitter alt="twitter" className=" cursor-pointer text-sm " />
         </div>
       </a>
       <a href="https://web3jobshunt.medium.com/">
-        <div className=" rounded-full border p-2">
+        <div className=" rounded-full border p-2 hover:bg-white hover:text-lightBlue">
           <BiLogoMedium alt="medium" className="cursor-pointer text-sm" />
         </div>
       </a>
       <a href="https://www.linkedin.com/company/web3jobhunt/">
-        <div className=" rounded-full border p-2">
+        <div className=" rounded-full border p-2 hover:bg-white hover:text-lightBlue">
           <BiLogoLinkedin
             alt="linkedin_icon"
             className=" cursor-pointer text-sm"
@@ -27,7 +23,7 @@ const SocialIcons = () => {
         </div>
       </a>{" "}
       <a href="https://t.me/Web3jobshunt">
-        <div className=" rounded-full border p-2">
+        <div className=" rounded-full border p-2 hover:bg-white hover:text-lightBlue">
           <BiLogoTelegram alt="telegram" className=" cursor-pointer text-sm" />
         </div>
       </a>
