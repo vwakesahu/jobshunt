4b8755e0fa015d582225eb8c7d4a54f1e6d6afa8
vwakesahu <89719144+vwakesahu@users.noreply.github.com>
2023-10-12 11:37:47 +0530

    Modified App Component added data to upper level

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Homepage from "./pages/Homepage";
+import FindJob from "./components/FindJob";
+import Navbar from "./components/Navbar";
+import { useStateValue } from "./context/StateProvider";
+import { data } from "autoprefixer";
+import { getAllJobData } from "./utils/firebaseFunctions";
+import { actionType } from "./context/reducer";
+import { useEffect } from "react";
 
 function App() {
+  const [{ jobData }, dispatch] = useStateValue();
+
+  const fetchData = async () => {
+    await getAllJobData().then((data) => {
+      dispatch({
+        type: actionType.SET_JOB,
+        jobData: data,
+      });
+    });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <div>
-     <Routes >
-      <Route path="/login" element={<Login />} />
-      <Route path="/*" element={<Homepage />} />
-     </Routes>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/*" element={<Homepage />} />
+
+        <Route path="/findjob" element={<FindJob />} />
+      </Routes>
     </div>
   );
 }
