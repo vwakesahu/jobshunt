77dc1b916c7e808dd4686ba88b28631594868b64
vwakesahu <89719144+vwakesahu@users.noreply.github.com>
2023-10-12 11:40:06 +0530

    UI changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Logo from "../img/logo.png";
 import MenuIcon from "../img/menu-icon.svg";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [isMenu, setIsMenu] = useState(false);
@@ -12,10 +13,13 @@ const Navbar = () => {
   return (
     <div className="p-4 md:p-5 md:px-48 bg-white font-medium">
       <div className=" flex items-center lg:justify-between md:justify-center justify-between md:gap-5">
-        <div className=" flex items-center md:text-lg ">
-          <img src={Logo} alt="logo" />
-          <p>eb3jobshunt</p>
-        </div>
+        <Link to="/">
+          {" "}
+          <div className=" flex items-center md:text-lg ">
+            <img src={Logo} alt="logo" />
+            <p>eb3jobshunt</p>
+          </div>{" "}
+        </Link>
 
         {/* for Desktop */}
         <div className="md:visible hidden md:flex gap-5">
