aab662004a8cbd14fafa9cb4c0252ee12f011e56
vwakesahu <viveksahu1762@gmail.com>
2023-11-09 13:43:54 +0530

    added emailjs

--- src/components/SubscribeComponent.jsx
+++ src/components/SubscribeComponent.jsx
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
 
 const SubscribeComponent = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_6bjaowx",
+        "template_9o3g8su",
+        form.current,
+        "NcULCO2MwqMaY-D7N"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          form.current.reset();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <div
       className="relative md:p-8 md:px-48 w-full p-7 bg-lightGray text-white md:py-24 flex md:flex-row flex-col gap-6 justify-center"
@@ -17,26 +41,42 @@ const SubscribeComponent = () => {
         </p>
       </div>
       <div className="hidden md:flex h-auto ">
-        <input
-          type="text"
-          className="min-w-[300px] px-6 py-3 border-[0.2px] border-lighttextGray bg-transparent border-r-0 rounded-l-lg focus:outline-none"
-          placeholder="Enter Your Email"
-        />
-        <button className="px-6 py-3 bg-lightBlue rounded-r-lg border border-lightBlue">
-          Subscribe
-        </button>
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+          action="#"
+          method="POST"
+          className=" w-full"
+        >
+          <input
+            type="text"
+            className="min-w-[300px] px-6 py-3 border-[0.2px] border-lighttextGray bg-transparent border-r-0 rounded-l-lg focus:outline-none"
+            placeholder="Enter Your Email"
+          />
+          <button className="px-6 py-3 bg-lightBlue rounded-r-lg border border-lightBlue">
+            Subscribe
+          </button>
+        </form>
       </div>
 
       {/* FOR MOBILES */}
       <div className="md:hidden">
-        <input
-          type="text"
-          className="w-full bg-transparent border-[0.2px] border-lighttextGray rounded-lg py-2 focus:outline-none px-3 "
-          placeholder="Enter Your Email"
-        />
-        <button className="w-full bg-lightBlue px-3 py-2 rounded-lg mt-2">
-          Subscribe Now
-        </button>
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+          action="#"
+          method="POST"
+          className=" w-full"
+        >
+          <input
+            type="text"
+            className="w-full bg-transparent border-[0.2px] border-lighttextGray rounded-lg py-2 focus:outline-none px-3 "
+            placeholder="Enter Your Email"
+          />
+          <button className="w-full bg-lightBlue px-3 py-2 rounded-lg mt-2">
+            Subscribe Now
+          </button>
+        </form>
       </div>
     </div>
   );
