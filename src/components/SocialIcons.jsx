822de65fc1bdf9fb08494891a1a88a05731a1fb1
vwakesahu <viveksahu1762@gmail.com>
2023-11-09 13:43:28 +0530

    changes URI

--- src/components/SocialIcons.jsx
+++ src/components/SocialIcons.jsx
@@ -4,17 +4,17 @@ import { FaXTwitter } from "react-icons/fa6";
 const SocialIcons = () => {
   return (
     <div className="flex gap-3">
-      <a href="https://twitter.com/web3jobshunt">
+      <a href="https://twitter.com/web3jobshunt" target="_blank">
         <div className=" rounded-full border p-2 hover:bg-white hover:text-lightBlue">
           <FaXTwitter alt="twitter" className=" cursor-pointer text-sm " />
         </div>
       </a>
-      <a href="https://web3jobshunt.medium.com/">
+      <a href="https://web3jobshunt.medium.com/" target="_blank">
         <div className=" rounded-full border p-2 hover:bg-white hover:text-lightBlue">
           <BiLogoMedium alt="medium" className="cursor-pointer text-sm" />
         </div>
       </a>
-      <a href="https://www.linkedin.com/company/web3jobhunt/">
+      <a href="https://www.linkedin.com/company/web3jobhunt/" target="_blank">
         <div className=" rounded-full border p-2 hover:bg-white hover:text-lightBlue">
           <BiLogoLinkedin
             alt="linkedin_icon"
@@ -22,7 +22,7 @@ const SocialIcons = () => {
           />
         </div>
       </a>{" "}
-      <a href="https://t.me/Web3jobshunt">
+      <a href="https://t.me/Web3jobshunt" target="_blank">
         <div className=" rounded-full border p-2 hover:bg-white hover:text-lightBlue">
           <BiLogoTelegram alt="telegram" className=" cursor-pointer text-sm" />
         </div>
