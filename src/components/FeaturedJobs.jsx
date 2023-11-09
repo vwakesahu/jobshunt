423a83137f49f07f7c2aa9a0574274d5656f3a3e
vwakesahu <viveksahu1762@gmail.com>
2023-11-09 13:39:59 +0530

    Worked on filtering

--- src/components/FeaturedJobs.jsx
+++ src/components/FeaturedJobs.jsx
@@ -1,20 +1,30 @@
 import React from "react";
 import HomeCard from "./HomeCard";
-
 import Avatar1 from "../img/avatars/av1.json";
 import Avatar2 from "../img/avatars/av2.json";
 import { useStateValue } from "../context/StateProvider";
+import { Link } from 'react-router-dom';
 
 const FeaturedJobs = () => {
   const [{ jobData }, dispatch] = useStateValue();
 
+  // Filter featured jobs
+  const featuredJobs = jobData?.filter((n) => n.featured === true)?.slice(0, 3);
+
   return (
-    <div className=" mt-16">
+    <div className=" mt-16 w-full">
       <p className=" text-2xl font-medium  text-center">Featured Jobs</p>
 
-      <HomeCard data={jobData?.filter((n) => n.featured === "True")} />
+      <HomeCard data={featuredJobs} />
 
-      
+      <div className="flex items-end justify-end">
+        {/* Use Link from react-router-dom to navigate to /findjob with the featured filter */}
+        <Link to="/findjob?featured=true">
+          <button className="mr-0 mt-3 hover:text-lightPrimary cursor-pointer">
+            View All
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
